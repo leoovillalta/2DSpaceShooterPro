742a4bf09b3ec830c0c1ce09f11804ef9be174f9
leoovillalta <leonardovillalta2511@gmail.com>
2020-08-22 02:18:52 -0600

    Homing Missiles fired by Boss final phase implemented

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -9,7 +9,7 @@ public class Boss : MonoBehaviour
     //health, anim, speed rotation for cannons, speed for lasers to follow
     //array of attacks
     [SerializeField]
-    public int _health=5;
+    public int _health=4;
     public int bossPhase=0;
     private GameObject _rightTurret;
     private GameObject _leftTurret;
@@ -20,6 +20,9 @@ public class Boss : MonoBehaviour
     //Turret Class
     //Laser Class
     // Boss Missile Class
+
+    [SerializeField]
+    private GameObject _smallExplosionPrefab;
     private float _startBossTimer = 5.0f;
     [SerializeField]
     private bool _startBossFight = false;
@@ -38,6 +41,7 @@ public class Boss : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        transform.GetComponent<BoxCollider2D>().enabled = false;
         _bossAnim = transform.GetComponent<Animator>();
         _conditionsForEndPhase = new bool[2];
         GetGamebjects();
@@ -52,6 +56,7 @@ public class Boss : MonoBehaviour
         _rightLaser = transform.Find("RightLaser").gameObject;
         _leftLaser = transform.Find("LeftLaser").gameObject;
         _centerTurret = transform.Find("CentralCannon").gameObject;
+        _missileSpawnerPosition = transform.Find("MissileSpawner").gameObject;
     }
     void SetDisableAllWeapons()
     {
@@ -60,6 +65,7 @@ public class Boss : MonoBehaviour
         _rightLaser.GetComponent<LaserBoss>().BossDisableLasers();
         _leftLaser.GetComponent<LaserBoss>().BossDisableLasers();
         _centerTurret.GetComponent<Turret>().BossDisableTurret();
+        _missileSpawnerPosition.GetComponent<MissileSpawner>().DeactivateMissiles();
     }
     IEnumerator StartBossFightTimer()
     {
@@ -119,14 +125,19 @@ public class Boss : MonoBehaviour
                 _bossAnim.SetTrigger("HideLasers");
                 Array.Resize(ref _conditionsForEndPhase, 1);
                 _centerTurret.GetComponent<Turret>().Shields(false);
+                _missileSpawnerPosition.GetComponent<MissileSpawner>().ActivateMissiles();
                 _conditionsForEndPhase[0] = false;
                 //Central Turret Shields Down
                 //Central Cannon + Missiles
                 break;
             case 4:
+                Array.Resize(ref _conditionsForEndPhase, 1);
+                _conditionsForEndPhase[0] = false;
                 //Central Cannon
                 break;
             case 5:
+                Debug.Log("Final Phase, Destroy Main Ship");
+                transform.GetComponent<BoxCollider2D>().enabled = true;
                 //Destroy big ship
                 //Maybe Restore all fire power in one last stand
                 break;
@@ -159,6 +170,61 @@ public class Boss : MonoBehaviour
         }
         return _allConditionsCompleted;
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if ((other.tag == "Laser" && (other.transform.GetComponent<Laser>().GetGameObjectType() == Laser.gameObjectType.Player))
+           || (other.tag == "Missile" && other.transform.GetComponent<Missile>().GetFiredBy() == Missile.FiredBy.Player))
+        {
+            if (other.tag == "Missile")
+            {
+                transform.GetChild(0).gameObject.SetActive(false);
+                //explosion
+                GameObject newExplosion = Instantiate(_smallExplosionPrefab, transform.position, Quaternion.identity, this.transform);
+                Destroy(newExplosion, 2.5f);
+            }
+            Destroy(other.gameObject);
+        }
+        _health--;
+        healthStatus();
+        StartCoroutine(PaintDamage());
+    }
+    void healthStatus()
+    {
+        switch (_health)
+        {
+            case 4:
+                break;
+            case 3:
+                transform.GetChild(1).gameObject.SetActive(true);
+                break;
+            case 2:
+                transform.GetChild(2).gameObject.SetActive(true);
+                break;
+            case 1:
+                transform.GetChild(3).gameObject.SetActive(true);
+                break;
+        }
+        
+        if (_health <= 0)
+        {
+            //DestroyedAnimation
+            Destroy(this.gameObject);
+        }
+
+    }
+    IEnumerator PaintDamage()
+    {
+        for (int i = 0; i <= 2; i++)
+        {
+            transform.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+            yield return new WaitForSeconds(0.1f);
+            transform.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+            yield return new WaitForSeconds(0.1f);
+        }
+
+    }
+
     public void ReportFirstLaserDown()
     {
         _firstLaserDown = true;
---
+++ Assets/Scripts/Boss/MissileSpawner.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private bool _missilesActive=false;
+    [SerializeField]
+    private int _missilesToBeShot = 6;
+    [SerializeField]
+    private GameObject _missilePrefab;
+    private Boss _boss;
+    private bool _shootingMissile = false;
+    [SerializeField]
+    private float _waitBetweenMissiles = 3.0f;
+    private bool _endOfPhase = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _boss = transform.parent.gameObject.GetComponent<Boss>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_missilesActive)
+        {
+            if (!_shootingMissile && !_endOfPhase)
+            {
+                _shootingMissile = true;
+                shotMissiles();
+            }
+        }
+        if (_missilesToBeShot <= 0 && !_endOfPhase)
+        {
+            _endOfPhase = true;
+            //EndPhase
+            _boss.ReportDestroyed(0);
+        }
+    }
+    void shotMissiles()
+    {
+        bool direction = (Random.value > 0.5f);
+        MissileDeploy(direction);
+    }
+    void MissileDeploy(bool right)
+    {
+        if (right)
+        {
+            //Instatiate to the right
+            GameObject missileshot = Instantiate(_missilePrefab, transform.position, Quaternion.identity, this.transform);
+            missileshot.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+
+        }
+        else
+        {
+            GameObject missileshot = Instantiate(_missilePrefab, transform.position, Quaternion.identity, this.transform);
+            missileshot.transform.rotation = Quaternion.Euler(new Vector3(0, 0, -90));
+            //Instantiate to the left
+        }
+        _missilesToBeShot--;
+        StartCoroutine(WaitBetweenMissiles());
+
+    }
+    IEnumerator WaitBetweenMissiles()
+    {
+        yield return new WaitForSeconds(_waitBetweenMissiles);
+        
+        _shootingMissile = false;
+    }
+
+    
+    public void ActivateMissiles()
+    {
+        _missilesActive = true;
+    }
+    public void DeactivateMissiles()
+    {
+        _missilesActive = false;
+    }
+    public void RechargeMissiles(int missilesRecharged)
+    {
+        _missilesToBeShot = missilesRecharged;
+    }
+}
